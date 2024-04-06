1bd996c1d6d8d89773f5f82b6039ed20dab49302
cmolinier <camille.molinier@etudiant.univ-rennes1.fr>
2024-04-06 16:44:53 +0200

    door sprite repair

--- app/modules/ActionableEntities/Door.py
+++ app/modules/ActionableEntities/Door.py
@@ -1,20 +1,18 @@
-#Importation
 import pygame
-import pygame as pg
 from app.modules.ActionableEntities.ActionableEntities import ActionableEntities
 
 
-#Classe
 class Door(ActionableEntities):
     def __init__(self, game, pos):
         self.room_init = (255, 255, 255)
         self.room_arrival = (70, 70, 70)
-        self.pos = pos  # Position of the door (x, y)
+        self.pos = pos
         self.pos_arrival = (30, 570)
         self.game = game
         self.locked = False
-        self.collider = pygame.Rect(self.pos[0], self.pos[1], 50, 50)
-        self.sprite = pygame.image.load('./assets/environnement/Tiles/porte_ouverte.png')
+        self.collider = pygame.Rect(self.pos[0], self.pos[1], 96, 144)
+        self.sprite = pygame.transform.scale(pygame.image.load('./assets/environnement/Tiles/porte_ouverte.png'),
+                                             (96, 144))
 
     def Interact(self):
         if self.locked:
@@ -24,6 +22,4 @@ class Door(ActionableEntities):
             self.game.roomFactory.switch_room()
 
     def draw(self):
-        #self.game.screen.blit(self.sprite, (0, 0))
-        pg.draw.rect(self.game.screen, (0, 0, 0), (self.pos[0], self.pos[1], 50, 50))
-        pygame.draw.rect(self.game.screen, (255, 0, 0), self.collider, 5)
+        self.game.screen.blit(self.sprite, (self.pos[0], self.pos[1]))
