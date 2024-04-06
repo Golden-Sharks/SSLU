2ba5af81d5d3b1724a15e5ee5788d7cfd0f21020
@tletarnec <thomas.le-tarnec@univ-rennes1.fr>
2024-04-07 01:19:57 +0200

    Init_feature

--- app/modules/monstre/shirak.py
+++ app/modules/monstre/shirak.py
@@ -8,7 +8,7 @@ class Shirak(Monstre):
         self.life = 3
         self.interaction_zone_slime = pygame.Rect(pos[0]-55, pos[1]-60, 200, 200)  # Rectangle de base
         self.interaction_zone_player = pygame.Rect(pos[0]-55, pos[1]-60, 200, 200)  # Rectangle de base
-
+        self.following = False  # Flag to track following state
     def draw(self):
         if self.life>0:
             scaled_image = pygame.transform.scale(self.current_image, (102, 58))
@@ -22,5 +22,22 @@ class Shirak(Monstre):
         if status == "Attaque":
             print("Shirak says: 'Ouch! That hurts!'")
             self.life -= 1
+            self.following = True  # Start following on attack
         else:
             print("Shirak says: 'I am Shirak, the slime!'")
+
+    def update(self):
+        if self.following and self.life > 0:
+            # Move towards the player's x-position
+            player_center_x = self.game.player.pos[0]
+            slime_center_x = self.pos[0]
+            speed = 2  # Adjust speed as needed
+
+            if player_center_x > slime_center_x:
+                self.pos[0] += min(speed, player_center_x - slime_center_x)  # Move right
+            elif player_center_x < slime_center_x:
+                self.pos[0] -= min(speed, slime_center_x - player_center_x)  # Move left
+
+            # Update interaction zones based on new position
+            self.interaction_zone_slime[0] = self.pos[0] - 55
+            self.interaction_zone_player[0]= self.pos[0] - 55
