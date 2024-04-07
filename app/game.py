01d110ffbf37f9113f869000ec313d9fb4b3887f
Spraduss <louis.ruellou@gmail.com>
2024-04-07 03:52:43 +0200

    Main menu and player life bar

--- app/game.py
+++ app/game.py
@@ -20,6 +20,7 @@ class Game:
         self.text = TextDisplay(self, self.get_db())
         self.currentRoom: Room = TutorialRoom(self)
         self.player = Player(self, [45, self.currentRoom.ground_collider.top+58])
+        self.image = pygame.transform.scale(pygame.image.load("assets/logo_goldenSharks.png"), (200, 200))
 
     def get_db(self):
         with open('./data/narrateur.json', 'r', encoding="utf-8") as file:
@@ -35,9 +36,36 @@ class Game:
         self.player.draw()
         pygame.display.update()
 
+    def drawMenuScreen(self):
+        self.screen.fill((50, 50, 50))
+        self.screen.blit(self.image, (400, 50))
+        title = "Super Shark Lab Undercover"
+        text_surface_title = pygame.font.Font(None, 50).render(title, False, (15, 5, 107))
+        self.screen.blit(text_surface_title, (250, 300))
+        menuText = "Press Enter to start the game or Q to quit"
+        text_surface_instructions = pygame.font.Font(None, 36).render(menuText, False, (15, 5, 107))
+        self.screen.blit(text_surface_instructions, (260, 500))
+
+        pygame.display.update()
+
+
+
     def run(self):
-        # Changing surface color
         pygame.display.flip()
+        isInMenu = True
+
+        while isInMenu:
+            self.drawMenuScreen()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    isInMenu = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        isInMenu = False
+                    elif event.key == pygame.K_q:
+                        self.running = False
+                        isInMenu = False
 
         while self.running:
             self.draw()
